09e6052d485cca306890d816845156b2689f84d1
Dheeraj Sachdeva <dheerajarya@gmail.com>
2023-02-02 16:56:25 +0530

    form validation

--- script.js
+++ script.js
@@ -204,3 +204,15 @@ closemodalbuttons.addEventListener('click', () => {
   // overlay.style.display = 'none';
   window.location.reload();
 });
+
+function emailValidator() {
+  const email = document.getElementById('email').value;
+  const emailLowcase = email.toLowerCase();
+  const EMAIL_INVALID = `Please enter a correct format of " ${email} " your address in lowercase format like this: <b>${emailLowcase}</b> Thank you`;
+  const emailRegx = /^([a-z\d-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
+
+  document.forms[0].onsubmit = function (event) {
+    if (emailRegx.test(email)) {
+      document.getElementById('valTxt').innerHTML = 'Success!';
+      document.getElementById('valTxt').style.color = 'green';
+    }
